e983fc056e49df03bceebdc866cdc69a1f0d5004
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2020-03-08 20:45:26 -0700

    Update and upload

---
+++ README.md
@@ -0,0 +1,9 @@
+## Explorer from Windows 10 made with React
+
+Only for demonstration purposes
+
+[Demo](https://peyty.github.io/react-explorer-web/)
+
+![Screenshot](public/screenshot.png?raw=true)
+
+Design and copyright by Microsoft
---
+++ docs/precache-manifest.211213529344ef3621ee730d8da681a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac42974c9647c05eaa26e70c9b9a375",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0de23dd52284bc5fc8cf",
+    "url": "./static/css/main.0007e06d.chunk.css"
+  },
+  {
+    "revision": "0dc08c953d32cc32a73d",
+    "url": "./static/js/2.6ec907fd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.6ec907fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de23dd52284bc5fc8cf",
+    "url": "./static/js/main.761c041b.chunk.js"
+  },
+  {
+    "revision": "c1695ab4d889627100b5",
+    "url": "./static/js/runtime-main.9ec9ac37.js"
+  }
+]);
